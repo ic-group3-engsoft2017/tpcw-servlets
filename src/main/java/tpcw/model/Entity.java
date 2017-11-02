36a93f3e66b9c9dedcd8ba082dd1098bab3f1d85
Grupo 3 - Luiz Henrique Pegoraro <pegorarohluiz@gmail.com>
2017-11-02 15:28:41 -0200

    Added Entity and CachableEntity

---
+++ src/main/java/tpcw/model/Entity.java
@@ -0,0 +1,14 @@
+package tpcw.model;
+
+import java.sql.ResultSet;
+
+public abstract class Entity {
+
+
+    public Entity(ResultSet resultSet) {
+
+    }
+
+    public abstract int hashCode(Entity e);
+    public abstract boolean equals(Entity e);
+}
