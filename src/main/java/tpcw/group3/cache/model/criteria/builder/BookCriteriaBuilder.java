5a39d15c54c599611d16df68d6bd7f0bfc2b6350
Grupo 3 - Luiz Henrique Pegoraro <pegorarohluiz@gmail.com>
2017-11-19 17:54:04 -0200

    Adde a builder to BookCriteria to facilitate the creation of the objects.

---
+++ src/main/java/tpcw/group3/cache/model/criteria/builder/BookCriteriaBuilder.java
@@ -0,0 +1,38 @@
+package tpcw.group3.cache.model.criteria.builder;
+
+import tpcw.group3.cache.model.criteria.BookCriteria;
+
+public class BookCriteriaBuilder {
+
+    public static BookCriteriaBuilder builder() {
+        return new BookCriteriaBuilder();
+    }
+
+    private BookCriteria bookCriteria;
+
+    public BookCriteria build() {
+        return bookCriteria;
+    }
+
+    public BookCriteriaBuilder withSubject(String subject) {
+        bookCriteria.setSubject(subject);
+        return this;
+    }
+
+    public BookCriteriaBuilder withAuthorFirstName(String authorFirstName) {
+        bookCriteria.setAuthorFirstName(authorFirstName);
+        return this;
+    }
+
+    public BookCriteriaBuilder withAuthorLastName(String authorLastName) {
+        bookCriteria.setAuthorLastName(authorLastName);
+        return this;
+    }
+
+    public BookCriteriaBuilder withTitle(String title) {
+        bookCriteria.setTitle(title);
+        return this;
+    }
+
+
+}
