1ea83d1654bd7818f06582e5e734915bbbe941cf
Diego Galhardi <diegowsu@gmail.com>
2017-11-23 10:49:07 -0200

    Mapping Junit Tests servlets

--- src/test/java/tpcw/service/TPCW_ServiceTest.java
+++ src/test/java/tpcw/service/TPCW_ServiceTest.java
@@ -4,43 +4,104 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * Classe para cobertura de testes dos seguintes servlets:
+ * TPCW_say_hello
+ * TPCW_admin_request_servlet
+ * TPCW_admin_response_servlet
+ * TPCW_product_detail_servlet
+ * TPCW_buy_request_servlet --> CANDIDATO A CACHE
+ * TPCW_execute_search
+ * TPCW_new_products_servlet
+ * TPCW_best_sellers_servlet --> CANDIDATO A CACHE
+ * TPCW_order_display_servlet
+ * TPCW_buy_confirm_servlet
+ * TPCW_customer_registration_servlet
+ * TPCW_promotional_processing
+ * TPCW_shopping_cart_interaction --> CANDIDATO A CACHE
+ */
+
+
 public class TPCW_ServiceTest {
+	
+	/**
+	 * TPCW_say_hello
+	 * @throws Exception
+	 */
     @Test
     public void getName() throws Exception {
     }
 
+    /**
+     * TPCW_admin_request_servlet
+     * TPCW_admin_response_servlet
+     * TPCW_product_detail_servlet
+     * @throws Exception
+     */
     @Test
     public void getBook() throws Exception {
     }
 
+    /**
+     * TPCW_buy_request_servlet
+     * @throws Exception
+     */
     @Test
     public void getCustomer() throws Exception {
     }
 
+    /**
+     * TPCW_execute_search
+     * @throws Exception
+     */
     @Test
     public void doSubjectSearch() throws Exception {
     }
 
+    /**
+     * TPCW_execute_search
+     * @throws Exception
+     */
     @Test
     public void doTitleSearch() throws Exception {
     }
 
+    /**
+     * TPCW_execute_search
+     * @throws Exception
+     */
     @Test
     public void doAuthorSearch() throws Exception {
     }
 
+    /**
+     * TPCW_new_products_servlet
+     * @throws Exception
+     */
     @Test
     public void getNewProducts() throws Exception {
     }
 
+    /**
+     * TPCW_best_sellers_servlet
+     * @throws Exception
+     */
     @Test
     public void getBestSellers() throws Exception {
     }
 
+    /**
+     * TPCW_order_display_servlet
+     * @throws Exception
+     */
     @Test
     public void getMostRecentOrder() throws Exception {
     }
 
+    /**
+     * TPCW_buy_request_servlet
+     * @throws Exception
+     */
     @Test
     public void getCart() throws Exception {
     }
@@ -60,5 +121,58 @@ public class TPCW_ServiceTest {
     @Test
     public void getStock() throws Exception {
     }
+    
+    /**
+     * TPCW_buy_confirm_servlet
+     * @throws Exception
+     */
+    @Test
+    public void doBuyConfirm() throws Exception {   	
+    }
+    
+    /**
+     * TPCW_customer_registration_servlet
+     * @throws Exception
+     */
+    @Test
+    public void getUserName() throws Exception {  	
+    }
+    
+    /**
+     * TPCW_order_display_servlet
+     */
+    @Test
+    public void getPassword() throws Exception {
+    	
+    }
+    
+    /**
+     * TPCW_promotional_processing
+     * @throws Exception
+     */
+    @Test
+    public void getRelated() throws Exception {
+    	
+    }
+      
+    /**
+     * TPCW_shopping_cart_interaction
+     * @throws Exception
+     */
+    @Test
+    public void createEmptyCart() throws Exception {
+    	
+    }
+    
+    /**
+     * TPCW_shopping_cart_interaction
+     * @throws Exception
+     */
+    @Test
+    public void doCart() throws Exception {
+    	
+    }
+    
+    
 
 }
